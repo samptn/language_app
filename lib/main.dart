0328646cdf59ad5eb0cc95eb7fe4d645b0915743
Sabir Mondal <plutonappdev02@Plutons-Mac-mini.local>
2024-12-03 19:58:00 +0530

    Deployments checking...

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/bloc/home_bloc.dart';
 import 'package:flutter_application_1/routes/go_routes.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
